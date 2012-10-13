8dc0975490a7c83e73237a84be6ea696410dca0b
Mike Moore <mike@blowmage.com>
2012-10-13 16:22:47 -0600

    Apply design to quote display

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,2 +1,9 @@
 module AccountHelper
+  def tag_list quote
+    if quote.present? && quote.tags.present?
+      quote.tag_list.map do |tag|
+        link_to tag, tag_path(tag)
+      end.join(', ').html_safe
+    end
+  end
 end
