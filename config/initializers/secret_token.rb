cc68c1b33378de72a43e32a7dd00d4f39b99798c
Mike Moore <mike@blowmage.com>
2012-10-12 18:22:17 -0600

    Hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotebin::Application.config.secret_token = 'aa46821ac63c657bac0eebafad33cd76b056131a05b63ba5e9dc2cba46b478cf0f2ce94b3960f11ecef8cdad7ae906c42179e12928f84b49cdb46eca9d77aa97'
