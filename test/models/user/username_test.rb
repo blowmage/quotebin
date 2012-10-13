37c424fb5520451747e0f7c12c522805c453025a
Mike Moore <mike@blowmage.com>
2012-10-12 19:25:27 -0600

    Backfill User#username tests

--- test/models/user/username_test.rb
+++ test/models/user/username_test.rb
@@ -25,4 +25,44 @@ describe User, :username do
     assert_equal User.find_by_username("123456"), @mike
   end
 
+  it "can be set to a new, free value" do
+    @mike.username = "NOBODYHASTAKENTHISONEYET"
+    assert @mike.valid?
+  end
+
+  it "is unique across users" do
+    david = users :david
+    @mike.username = david.username
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("has already been taken")
+  end
+
+  it "only allows alphanumeric characters" do
+    @mike.username = "-blowmage"
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("can only have letters, numbers and '_'")
+
+    @mike.username = "blowm☣ge"
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("can only have letters, numbers and '_'")
+  end
+
+  it "cannot be a reserved username" do
+    @mike.username = Reserved::USERNAMES.sample
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("is reserved")
+  end
+
+  it "must be 3 characters or larger" do
+    @mike.username = "a"
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("is too short (minimum is 3 characters)")
+  end
+
+  it "must be 32 characters or smaller" do
+    @mike.username = "abcdefghijklmnopqrstuvwxyz0123456789"
+    refute @mike.valid?
+    assert @mike.errors[:username].include?("is too long (maximum is 32 characters)")
+  end
+
 end
