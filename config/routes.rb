c490267f264d7bb9e620553a530cfefa02e94cc1
Mike Moore <mike@blowmage.com>
2012-10-12 22:18:22 -0600

    Add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Quotebin::Application.routes.draw do
   root to: "home#index"
   resources :quotes
+  # Profile
+  get "profile" => "home#profile", as: :profile
+  # Devise
   devise_for :users
+  devise_scope :user do
+    # Sessions
+    get    'login'  => 'devise/sessions#new',     :as => :new_user_session
+    post   'login'  => 'devise/sessions#create',  :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    # Password
+    post   'reset_password'      => 'devise/passwords#create', as: :user_password
+    get    'reset_password/new'  => 'devise/passwords#new',    as: :new_user_password
+    get    'reset_password/edit' => 'devise/passwords#edit',   as: :edit_user_password
+    put    'reset_password'      => 'devise/passwords#update', as: :user_password
+    # Registration
+    get    'register'       => 'devise/registrations#new',     as: :new_user_registration
+    post   'register'       => 'devise/registrations#create',  as: :new_user_registration
+    get    'profile/edit'   => 'devise/registrations#edit',    as: :edit_user_registration
+    post   'profile'        => 'devise/registrations#create',  as: :user_registration
+    put    'profile'        => 'devise/registrations#update',  as: :user_registration
+    delete 'profile'        => 'devise/registrations#destroy', as: :user_registration
+    get    'profile/cancel' => 'devise/registrations#cancel',  as: :cancel_user_registration
+  end
 end
