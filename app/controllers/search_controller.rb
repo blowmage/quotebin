7759b9d1dae5763531efdad90f6a4245b41a3763
Mike Moore <mike@blowmage.com>
2012-10-14 13:06:24 -0600

    Update search bar results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,12 +12,11 @@ class SearchController < ApplicationController
   def autocomplete
     payload = []
     if results.total_count > 0
-      payload << { value: query, label: "There are #{results.total_count} results for \"#{query}\"".html_safe, url: search_path(q: query) }
-      results.limit(8).each do |q|
-        payload << { value: query, label: "<em>\"#{truncate(q.quotation, length: 60)}\"</em> by <strong>#{q.author}</strong>".html_safe, url: account_quote_path(q.owner.username, q.id) }
+      payload = results.limit(8).map do |q|
+        { value: query, label: "<em>\"#{truncate(q.quotation, length: 60)}\"</em> by <strong>#{q.author}</strong>".html_safe, url: account_quote_path(q.owner.username, q.id) }
       end
-      payload << { value: query, label: "<strong>Search for <em>\"#{query}\"</em>".html_safe, url: search_path(q: query) }
-      payload << { value: query, label: "<strong>Create a quote for <em>\"#{query}\"</em>".html_safe, url: new_quote_path }
+      payload << { value: query, label: "<strong>Search for</strong> <em>\"#{query}\"</em> (#{results.total_count} results)".html_safe, url: search_path(q: query) }
+      payload << { value: query, label: "<strong>Create a quote</strong> for <em>\"#{query}\"</em>".html_safe, url: new_quote_path }
     end
 
     render json: payload
