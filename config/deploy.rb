45f0e8883bfb688264ca961b94d81a50c6581076
Mike Moore <mike@blowmage.com>
2013-01-08 15:21:53 -0700

    Update repository
    Move off the Rails Rumble repo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:railsrumble/r12-team-568.git"
+set :repository, "git@github.com:blowmage/quotebin.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
